52fb3879455464ca48413143d7d231b1a8e2dbd4
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-07-03 13:10:34 +0530

    Create Palindrome List.java

---
+++ Programming/Linked List/Palindrome List.java
@@ -0,0 +1,48 @@
+
+public class Solution {
+    public int lPalin(ListNode head) {
+        if (head == null || head.next == null) return 1;
+        int l = 0;
+        ListNode curr = head;
+        
+        while (curr != null) {
+            l++;
+            curr = curr.next;
+        }
+        
+        int i = 1;
+        curr = head;
+        
+        while (i < l/2) {
+            curr = curr.next;
+            i++;
+        }
+        
+        ListNode mid = reverse(curr.next);
+        
+        curr = head;
+        
+        while(mid != null) {
+            if (curr.val != mid.val) return 0;
+            curr = curr.next;
+            mid = mid.next;
+        }
+        
+        return 1;
+    }
+    
+    public ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+        
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+}
