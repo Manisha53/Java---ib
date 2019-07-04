9bad2f5951fa1540ebc17bc13f00c9f0fb8e151b
Manisha Pandey <37845282+Manisha53@users.noreply.github.com>
2019-07-04 11:37:59 +0530

    Create Length of Longest Subsequence.java

---
+++ Programming/DP/Length of Longest Subsequence.java
@@ -0,0 +1,46 @@
+public class Solution {
+    /* DO NOT MODIFY THE LIST. IT IS READ ONLY
+    
+    
+
+Given an array of integers, find the length of longest subsequence which is first increasing then decreasing.
+
+**Example: **
+
+For the given array [1 11 2 10 4 5 2 1]
+
+Longest subsequence is [1 2 10 4 2 1]
+
+Return value 6
+
+    
+    */
+    public int longestSubsequenceLength(final List<Integer> A) {
+        
+          int n=A.size();
+    int lis[]=new int[n];
+    int lds[]=new int[n];
+    for(int i=0;i<n;i++){
+     lis[i]=1;
+     lds[i]=1;
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(A.get(i).intValue() > A.get(j).intValue() && lis[i]<lis[j]+1)
+             lis[i]=lis[j]+1;
+        }
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int j=n-1;j>i;j--){
+            if(A.get(i).intValue() > A.get(j).intValue() && lds[i]<lds[j]+1)
+             lds[i]=lds[j]+1;
+        }
+    }
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {int res=lis[i]+lds[i]-1;
+     ans=Math.max(ans,res);
+    }
+return ans;
+    }
+}
